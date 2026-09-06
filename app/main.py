5d576fef920787e6d5d4be8193e42b57243384ec
Petro <petropavliuk12.05@gmail.com>
2026-09-06 21:49:05 +0300

    added check API is provided

--- app/main.py
+++ app/main.py
@@ -1,20 +1,23 @@
 import os
 import requests
 
-API_KEY = os.environ["API_KEY"]
+API_KEY = os.environ.get("API_KEY")
 WEATHER_URL = "http://api.weatherapi.com/v1/current.json"
 CITY = "Paris"
 
 
 def get_weather() -> None:
+    if not API_KEY:
+        print("API key not provided")
+        return
     response = requests.get(WEATHER_URL + f"?q={CITY}&key=" + API_KEY)
     if response.status_code == 200:
         data = response.json()
         current_weather = data["current"]["condition"]["text"]
-        current_time = data["location"]["localtime"]
+        local_time = data["location"]["localtime"]
         current_temp = data["current"]["temp_c"]
         result = (f"It's {current_weather} ({current_temp} °C) "
-                  f"in {CITY} at the {current_time} local time")
+                  f"in {CITY} at the {local_time} local time")
         print(result)
 
 
